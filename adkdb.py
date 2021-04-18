1044f9b01f03cd8fbe0086bbf3ddf4ad30066918
Roman Druzhkov <r.druzhkov99@gmail.com>
2021-04-18 16:34:54 +0300

    Implemented class for working with sqlite3 database.

---
+++ adkdb.py
@@ -0,0 +1,56 @@
+
+import sqlite3
+import logging
+from contextlib import contextmanager
+
+
+@contextmanager
+def cursor(database_name):
+    try:
+        con = sqlite3.connect(database_name)
+        cur = con.cursor()
+        yield cur
+    finally:
+        con.commit()
+        con.close()
+
+
+class AdkDatabase:
+
+    def __init__(self):
+        self.__database_name = 'database.db'
+        self.__table_name = 'Auth'
+
+        if not self.__is_table_present():
+            logging.debug('Database table is not present.')
+            self.__create_table()
+
+    def __is_table_present(self):
+        with cursor(self.__database_name) as cur:
+
+            cur.execute(
+                f''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='{self.__table_name}' '''
+            )
+            if cur.fetchone()[0] == 1:
+                is_table_present = True
+            else:
+                is_table_present = False
+
+        return is_table_present
+
+    def __create_table(self):
+        logging.debug('Creating table...')
+        with cursor(self.__database_name) as cur:
+            cur.execute(f'create table {self.__table_name}(login text unique, passwordHash text)')
+        logging.debug('Table created...')
+
+    def insert(self, login: str, password_hash: str):
+        with cursor(self.__database_name) as cur:
+            cur.execute(f'insert into {self.__table_name} values (?, ?)', (login, password_hash))
+
+    def get_hash(self, login: str):
+        with cursor(self.__database_name) as cur:
+            cur.execute(f'select passwordHash from {self.__table_name} where login=?', (login, ))
+            result = cur.fetchone()[0]
+
+        return result
